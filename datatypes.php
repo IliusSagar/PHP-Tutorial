5623a76b66a1235e2786401099743b5ca632480b
IliusSagar <sagar01830@gmail.com>
2021-09-17 22:03:51 -0700

    variables learn

---
+++ datatypes.php
@@ -0,0 +1,27 @@
+<?php
+
+
+	$x = "EAP Software";
+	// $x = 2500;
+	// $x = "2500";
+	// $x = '2500';
+
+	// $x = "2500.50";
+	// $x = 2500.50;
+
+	// $x = 2500.50;
+
+	// $x = true;
+	// $x = false;
+
+	// $x = array("html","css","js");
+	$x = null;
+
+	echo $x . "<br>";
+
+	// echo $x[1] . "<br>";
+
+	var_dump($x);
+	
+
+?>
