ae18f18b36be993224d4fd3f6d74d6009f82f9c8
IliusSagar <sagar01830@gmail.com>
2021-09-17 20:29:46 -0700

    echo learn

---
+++ echo.php
@@ -0,0 +1,20 @@
+<?php
+
+
+	// echo "EAP Software";
+	// echo 'EAP Software';
+	// echo ('EAP Software');
+	// echo "EAP", " Software";
+	// echo "EAP". " Software";
+	// echo "<b>EAP". " Software</b>";
+	// echo "<h1><i>EAP". " Software</i></h1>";
+	// echo 23.58;
+
+
+	 // print "<h1><i>EAP", " Software</i></h1>";
+	 // print 23.58;
+
+	print "<h1><i>EAP". " Software</i></h1>";
+	 print 23.58;
+
+?>
