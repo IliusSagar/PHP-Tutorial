e9263b723a693adae9da39c6f0b534b54e085ef9
IliusSagar <sagar01830@gmail.com>
2021-09-17 20:46:38 -0700

    variables learn

---
+++ variables.php
@@ -0,0 +1,26 @@
+<?php
+
+	// $name = "EAP Software";
+	// echo $name;
+
+
+	// $name = "EAP Software<br>";
+	// echo $name;
+	// echo $name;
+	// echo $name;
+	// echo $name;
+
+	
+	// $name = "EAP Software<br>";
+	// echo "<h1>" . $name . "</h1>";
+
+
+	$name = "EAP Software<br>";
+	$num = 2358;
+	echo "Hello how are you : " . $name;
+	echo $num;
+
+
+
+
+?>
